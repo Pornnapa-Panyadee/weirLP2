b8d494d16c0725b9827579c28c3ca101ae29a6b6
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2024-05-11 20:23:14 +0700

    open link report

--- routes/web.php
+++ routes/web.php
@@ -71,17 +71,17 @@ Route::POST('/report/sediment_upconcrete/pdf', 'App\Http\Controllers\ReportPDFCo
 
 // Report Home 
 // Route::get('/report/map', 'App\Http\Controllers\ReportPDFController@reportpdf_warning'); 
-Route::get('/report/chart', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
-Route::get('/report/scoreComposition', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
-Route::get('/report/problem', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
+//Route::get('/report/chart', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
+//Route::get('/report/scoreComposition', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
+//Route::get('/report/problem', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
 Route::get('/report/sedimentTable', 'App\Http\Controllers\ReportPDFController@reportpdf_warning');
 
 Route::get('/report/sediment', 'App\Http\Controllers\MapScoreController@sedimentscore');
 Route::get('/report/sedimentTable', function () {return view('pages.sediment_table');});
 Route::get('/report/map', 'App\Http\Controllers\MapScoreController@scoretable');
-// Route::get('/report/chart', 'App\Http\Controllers\ChartReportController@score');
-// Route::get('/report/scoreComposition', function () {return view('pages.scorelist');});
-// Route::get('/report/problem', function () {return view('pages.problemlist');});
+Route::get('/report/chart', 'App\Http\Controllers\ChartReportController@score');
+Route::get('/report/scoreComposition', function () {return view('pages.scorelist');});
+Route::get('/report/problem', function () {return view('pages.problemlist');});
 
 
 // add image
